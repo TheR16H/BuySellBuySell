e4d31148b843fc46a0c6d6161e686c229ef576d8
TheR16H <rashawn5hall@gmail.com>
2024-08-08 16:44:46 -0400

    plain product columns

--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -8,8 +8,22 @@ class Product extends Model {}
 
 // set up fields and rules for Product model
 Product.init(
-  {
-    // define columns
+  { product_name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  price: {
+    type: DataTypes.DECIMAL(10, 2), // Assuming price is a decimal number
+    allowNull: false
+  },
+  stock: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
+  category_id: {
+    type: DataTypes.INTEGER,
+    allowNull: false
+  },
   },
   {
     sequelize,
