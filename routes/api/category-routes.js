9e47b35b84b80b6939a5bb87b6cec389fe004609
TheR16H <rashawn5hall@gmail.com>
2024-08-11 02:54:32 -0400

    cateRoutes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -11,20 +11,46 @@ try {
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+ try {
+  const categoryData = await Category.findByPk(req.params.id, { include: [{ model: Product }]});
+ res.status(200).json(categoryData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.post('/', (req, res) => {
-  // create a new category
-});
+try {
+  const newCategory = await Category.create(req.body);
+  res.status(200).json(newTag);
+  } catch(err) {
+    res.status(500).json(err);
+  }});
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+try { 
+  const updatedCategory = await Category.update(
+    { category_name: req.body.category_name},
+    { where: { id: req.params.id}
+  }
+  )
+  res.status(200).json(updatedCategory);
+  } catch(err) {
+    res.status(500).json(err);
+}
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  try {
+    const deletedTag = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    })
+    res.status(200).json(deletedCategory);
+  } catch(err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
